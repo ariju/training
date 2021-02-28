2fae463903a5ad98d11772a13e7887144edc1b60
ariju <jun.0309ari@gmail.com>
2021-02-28 17:34:10 +0900

    カレンダーの作成

---
+++ pages/calendar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-app-bar color="blue" dark>
+      <v-toolbar-title>work out</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-btn nuxt-link to="/" class="m-5">トップページ</v-btn>
+    </v-app-bar>
+    <v-main>
+      <Calendar />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Calendar from "~/components/Calendar";
+
+export default {
+  name: "App",
+  components: {
+    Calendar
+  },
+  data() {
+    return {};
+  }
+};
+</script>
