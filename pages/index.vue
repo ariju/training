79cee2d895845803a91b42fbc2eb5f83a9cd1490
ariju <jun.0309ari@gmail.com>
2021-02-28 17:43:29 +0900

    トップページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app">
-    <h3>データの管理</h3>
-    <label for="name">ニックネーム</label>
-    <input 
-      id="name" 
-      type="text" 
-      v-model="name"
-    >
-    <br><br>
-    <label for="comment">コメント</label>
-    <textarea 
-      id="comment" 
-      v-model="comment"
-    ></textarea>
-    <h2>データ</h2>
-    <pre><p>{{ name }}</p></pre>
-    <pre><p>{{ comment }}</p></pre>
-  </div>
-</template>
+  <v-app>
+    <div>
+      <v-app-bar color="red" dark>
+        <v-toolbar-title>work out</v-toolbar-title>
+        <div class="flex-grow-1"></div>
+        <v-btn @click="logout">ログアウト</v-btn>
 
+        <v-btn nuxt-link to="/calendar" class="m-5">カレンダー</v-btn>
+        <v-btn nuxt-link to="/management" class="m-5">管理</v-btn>
+      </v-app-bar>
+      <img src="~assets/images/spine.jpg" class="w-full" />
+    </div>
+    <v-footer>
+      <div class="flex-grow-1"></div>
+      <div>
+        &copy; 2021 workout
+      </div>
+    </v-footer>
+  </v-app>
+</template>
 
 <script>
 export default {
-  data() {
-    return {
-      name: "",
-      comment: "",
+  middleware: ["checkAuth"],
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+      this.$router.push("/login");
+    },
+    deleteUser() {
+      this.$store.dispatch("delete");
     }
   }
-}
+};
 </script>
