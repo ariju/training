911c822e92a5d42a3f0b8263e0c8c7c328d76d3c
ariju <jun.0309ari@gmail.com>
2021-03-03 04:31:10 +0900

    レイアウトの変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,8 @@
 <template>
   <v-app>
     <div>
-      <v-app-bar color="red" dark>
-        <v-toolbar-title>work out</v-toolbar-title>
-        <div class="flex-grow-1"></div>
-        <v-btn @click="logout">ログアウト</v-btn>
-
-        <v-btn nuxt-link to="/calendar" class="m-5">カレンダー</v-btn>
-        <v-btn nuxt-link to="/management" class="m-5">管理</v-btn>
-      </v-app-bar>
       <img src="~assets/images/spine.jpg" class="w-full" />
     </div>
-    <v-footer>
-      <div class="flex-grow-1"></div>
-      <div>
-        &copy; 2021 workout
-      </div>
-    </v-footer>
   </v-app>
 </template>
 
@@ -28,9 +14,6 @@ export default {
       this.$store.dispatch("logout");
       this.$router.push("/login");
     },
-    deleteUser() {
-      this.$store.dispatch("delete");
-    }
   }
 };
 </script>
