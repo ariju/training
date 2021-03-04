d24ae5160ebcb9e6354fe723d6cffa33cf522c9c
ariju <jun.0309ari@gmail.com>
2021-03-05 03:41:35 +0900

    トップページのレイアウト

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <v-app>
-    <div>
-      <img src="~assets/images/spine.jpg" class="w-full" />
-    </div>
-  </v-app>
+  <section class="home">
+    <v-container fluid fill-height class="home-content">
+      <v-row>
+        <v-col class="home-content-text">
+          <p>Thank you for your hard work today</p>
+          <p>Record today's achievements</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
 </template>
 
 <script>
@@ -13,7 +18,24 @@ export default {
     logout() {
       this.$store.dispatch("logout");
       this.$router.push("/login");
-    },
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home-content {
+  background: url("../assets/images/spine.jpg");
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
+
+  &-text {
+    color: white;
+    text-align: center;
+    font-size: 28px;
+    font-weight: bold;
+  }
+}
+</style>
