140caaa1ce585316dd6443aaf90e6e0a4891d753
ariju <jun.0309ari@gmail.com>
2021-03-21 14:06:26 +0900

    修正

--- middleware/checkLogin.js
+++
@@ -1,6 +0,0 @@
-export default async function ({ redirect, app }) {
-  if (await app.$auth()) {
-    // ログイン中だったらTOPページに遷移する
-    redirect('/')
-  }
-}
--- middleware/checkRegister.js
+++
@@ -1,6 +0,0 @@
-export default async function({ redirect, app }) {
-  if (await app.$user()) {
-    // すでにアカウント作成済の場合はTOPページにリダイレクト
-    redirect('/')
-  }
-}
